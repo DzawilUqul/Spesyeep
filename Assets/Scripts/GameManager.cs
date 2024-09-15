13e199e0c52d919a38c68d885e34e936168bae4d
Fernandotambun <1112022214723@gmail.com>
2024-09-15 20:26:15 +0700

    git init

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro; 
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public int score = 0; 
-    public TextMeshProUGUI scoreText;    public static GameManager instance;
+    public TextMeshProUGUI scoreText;    
+    public static GameManager instance;
+
+    public GameObject gameOver, heart1, heart2, heart3;
+
+    public static int health;
+
+    void Start()
+    {
+        health = 3;
+        
+        heart1.gameObject.SetActive(true);
+        heart2.gameObject.SetActive(true);
+        heart3.gameObject.SetActive(true);
+        gameOver.gameObject.SetActive(false);
+    }
+
+    void Update()    
+    {
+        switch (health)
+        {
+            case 3:
+                Debug.Log("3");
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                break;
+            case 2:
+            Debug.Log("2");
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(false);
+                break;
+            case 1:     
+            Debug.Log("1");            
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                break;
+            default:
+                heart1.gameObject.SetActive(false);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                gameOver.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                break;                        
+        }
+    }
     private void Awake()
     {
         // If there is no existing instance of GameManager
@@ -29,5 +77,5 @@ public class GameManager : MonoBehaviour
         score += points;
         scoreText.text = "Score : " + score.ToString();
     }
-
+    
 }
---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class RestartGame : MonoBehaviour
+{
+    public GameObject gameOver;
+    public void RestartLevel()
+    {
+        Debug.Log("Restarting game");        
+        Time.timeScale = 1;
+        GameManager.health = 3;
+        // ScoreManager.scoreCount = 0; // gak mau reset score
+        gameOver.gameObject.SetActive(false);
+    }
+}
