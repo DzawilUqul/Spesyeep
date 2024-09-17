96f46ef99a869ec2b7b9933624f8f7c7335a4d07
Fernandotambun <1112022214723@gmail.com>
2024-09-17 22:47:26 +0700

    Add Animate Eat

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public static GameManager Instance; //1
+    [SerializeField] private GameObject playerEatImage;
+    [SerializeField] private GameObject playerNotEatImage;
+
+    private bool isPlayerEat = false;
+
+    private void Start()
+    {
+        playerEatImage.SetActive(false);
+        playerNotEatImage.SetActive(true);
+    }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
@@ -13,6 +23,16 @@ public class PlayerCollision : MonoBehaviour
             Debug.Log("Hit Obstacle");
             
             Destroy(other.gameObject);
+            if (isPlayerEat == false)
+            {
+                StartCoroutine(PlayerEatImage());
+                isPlayerEat = true;
+            }
+            else
+            {
+                isPlayerEat = false;
+            }
+
 
             GameManager.instance.AddScore(1);
         }
@@ -22,5 +42,15 @@ public class PlayerCollision : MonoBehaviour
             Debug.Log("Trigger Line Reached!");
         }
     }
-    
+
+    // generate coroutine to delay the player eat image
+    private IEnumerator PlayerEatImage()
+    {
+        playerEatImage.SetActive(true);
+        playerNotEatImage.SetActive(false);
+        yield return new WaitForSeconds(.5f);
+        playerEatImage.SetActive(false);
+        playerNotEatImage.SetActive(true);
+        isPlayerEat = false;
+    }
 }
